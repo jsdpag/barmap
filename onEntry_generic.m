b0bf78ca3d61f14cfe25199eaa75f43dc1247c9e
smithj <jackson.smith@esi-frankfurt.de>
2023-02-03 15:53:19 +0100

    Send human readable trial outcome to Synapse run-time note. Do this instead of sending numeric trial error code.

--- onEntry_generic.m
+++ onEntry_generic.m
@@ -99,10 +99,10 @@ function  onEntry_generic( a )
     
     % SynapseAPI is live, send a run-time note with the trial outcome
     if  a.synflg  &&  ~ a.syn.setParameterValue( 'RecordingNotes' , ...
-        'Note' , sprintf( 'Outcome %s' , a.TrialError ) )
+        'Note' , sprintf( 'Outcome %s' , a.State.name ) )
         
-      error( 'Failed to deliver run-time note to Synapse: Outcome %d' , ...
-        a.TrialError )
+      error( 'Failed to deliver run-time note to Synapse: Outcome %s' , ...
+        a.State.name )
       
     end
   end % Trial error code
