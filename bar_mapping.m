82d012c6372b369adf757550d712dfc2cf83524a
Jackson Smith <jackson.smith@esi-frankfurt.de>
2023-02-03 14:51:50 +0000

    Revert "Guarantee that window buffer Gizmo timers run down between trials."
    This reverts commit 629fb4889699380a84b79a5fa23933bd6901ad8e

--- bar_mapping.m
+++ bar_mapping.m
@@ -131,12 +131,10 @@ if  TrialData.currentTrial == 1
     % fixation window will be made on first trial.
     P.FixTol = NaN ;
   
-  % Make tic time measurement at end of Wait and cleanUp states, for ITI
-  P.WaitEnd  = StateRuntimeVariable ;
+  % Make tic time measurement at end of previous trial for ITI measure
   P.ITIstart = StateRuntimeVariable ;
   
-    % Initialise to zero, so that we don't wait on first trial
-     P.WaitEnd.value = zeros( 1 , 'uint64' ) ;
+    % Initialise P.ITIstart to zero, so that we don't wait on first trial
     P.ITIstart.value = zeros( 1 , 'uint64' ) ;
   
   % Ideal duration of one sweep by the bar, in milliseconds
@@ -305,7 +303,7 @@ if  TrialData.currentTrial > 1  &&  diff( pre.blocks( end - 1 : end ) )
   
   % Explicitly release resources
   for  E = { 'StimServerAnimationDone' , 'BlinkStart' , 'BlinkEnd' , ...
-      'Bar' , 'Fix' , 'Motion' , 'WaitEnd' , 'ITIstart' }
+      'Bar' , 'Fix' , 'Motion' , 'ITIstart' }
     delete( P.( E{ 1 } ) ) ;
   end
   
@@ -423,10 +421,6 @@ end % simulation
   
   % Guarantee that bar motion animation is stopped
   ENDACT.BarOn = { @( ) P.Bar.stop_animation( ) } ;
-  
-  % Wait measures time so that we can guarantee that Synapse buffers linked
-  % to TdtWinBuf objects have finished counting down the response window.
-  ENDACT.Wait = { @( ) P.WaitEnd.set_value( tic ) } ;
   
   % cleanUp measures time that inter-trial-interval starts, then prints one
   % final message to show that all State objects have finished executing
@@ -587,18 +581,6 @@ end % synapse running
 %%% Complete previous trial's inter-trial-interval %%%
 
 sleep( ItiMinMs  -  1e3 * toc( P.ITIstart.value ) )
-
-% Using synapse
-if  P.UsingSynapse
-  
-  % Longest response window, in seconds
-  mrw = max( P.buf.spk.respwin , P.buf.mua.respwin ) ;
-  
-  % Guarantee that windowed buffer Gizmo response window timer has expired
-  % before launching new trial
-  sleep( 1e3 * ( mrw - toc( P.WaitEnd.value ) ) )
-  
-end % synapse
 
 
 %%% --- SCRIPT FUNCTIONS --- %%%
